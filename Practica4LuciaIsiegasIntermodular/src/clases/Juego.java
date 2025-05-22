e1d613d1bd270c39cc5cf6c77e927f867b090875
LuciaIsiegas <24.damt.luciaisiegas@fundacionmontessori.com>
2025-05-23 01:53:17 +0200

    Eliminacion de Errores innecesarios

--- Practica4LuciaIsiegasIntermodular/src/clases/Juego.java
+++ Practica4LuciaIsiegasIntermodular/src/clases/Juego.java
@@ -2,8 +2,6 @@ package clases;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
